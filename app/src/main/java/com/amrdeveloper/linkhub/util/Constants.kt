38fbdcc37d466f22251816529beabe05a7cbfed8
David Burns <djacidburn1877@gmail.com>
2023-11-30 00:29:36 -0500

    Update information
    Ready to build and publish

--- app/src/main/java/com/amrdeveloper/linkhub/util/Constants.kt
+++ app/src/main/java/com/amrdeveloper/linkhub/util/Constants.kt
@@ -2,10 +2,10 @@ package com.amrdeveloper.linkhub.util
 
 const val packageName = "com.amrdeveloper.linkhub"
 const val PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=$packageName"
-const val REPOSITORY_URL = "https://github.com/AmrDeveloper/LinkHub"
+const val REPOSITORY_URL = "https://github.com/djacidfx/LinkHub"
 const val REPOSITORY_ISSUES_URL = "$REPOSITORY_URL/issues"
 const val REPOSITORY_CONTRIBUTORS_URL = "$REPOSITORY_URL/graphs/contributors"
-const val REPOSITORY_SPONSORSHIP_URL = "https://github.com/sponsors/AmrDeveloper"
+const val REPOSITORY_SPONSORSHIP_URL = "https://donate.stripe.com/aEU8yle0n8c7b96cMN"
 
 const val WIDGET_REFRESH_ACTION = "${packageName}.action.REFRESH"
 const val WIDGET_ITEM_CLICK_ACTION = "${packageName}.action.ITEM_CLICK"
